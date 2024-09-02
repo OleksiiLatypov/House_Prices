919069d774f57f4f8e6aad6584eaad0b040747cb
oleksiilatypov <latypov.oleksii.la@gmail.com>
2024-09-02 23:58:31 +0300

    change estimator

--- utils/estimator.py
+++ utils/estimator.py
@@ -1,4 +1,6 @@
-from settings.constants import TRAIN_CSV, SAVED_SCALER
+from typing import Optional, Union
+
+from settings.constants import TRAIN_CSV, SAVED_MODEL
 from dataloader import Dataloader
 from sklearn.linear_model import LinearRegression
 import pandas as pd
@@ -10,18 +12,21 @@ from sklearn.ensemble import RandomForestRegressor, StackingRegressor
 from sklearn.preprocessing import MinMaxScaler, RobustScaler
 import joblib
 import numpy as np
-#from xgboost import XGBRegressor
+
+
+# from xgboost import XGBRegressor
 
 
 class Estimator:
-    def __init__(self, train_csv=None, loader=None, scaler=None, model=None):
+    def __init__(self, train_csv: Optional[Union[str, pd.DataFrame]] = None,
+                 loader=None, scaler=None, model=None) -> None:
         self.train_csv = train_csv if train_csv else pd.read_csv(TRAIN_CSV)
         self.loader = loader if loader else Dataloader()
         self.scaler = scaler if scaler else MinMaxScaler()
         self.model = model
         if self.model is None:
-            self.model = CatBoostRegressor(iterations=1800,
-                                           learning_rate=0.0191,
+            self.model = CatBoostRegressor(iterations=1150,
+                                           learning_rate=0.03,
                                            depth=5,
                                            l2_leaf_reg=1,
                                            loss_function='RMSE',
@@ -33,13 +38,19 @@ class Estimator:
         X, y = self.loader.load()
 
         pipeline = Pipeline([('scaler', self.scaler), ('model', self.model)])
+
         kf = KFold(n_splits=5, shuffle=True, random_state=49)
 
         cv_scores = cross_val_score(pipeline, X, y, cv=kf, scoring=make_scorer(mean_squared_error))
+
         rmse_scores = np.sqrt(cv_scores)
+
         mean_rmse = np.mean(rmse_scores)
+
         pipeline.fit(X, y)
-        joblib.dump(pipeline, '/workspaces/105818016/house_price/models/trained_pipeline.pkl')
+
+        joblib.dump(pipeline, SAVED_MODEL)
+
         print(rmse_scores)
         print(mean_rmse)
         print(X.shape)
