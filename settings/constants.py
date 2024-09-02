e9bf50dbcd1cfb8ead5792da42c95c4c8920bb5d
oleksiilatypov <latypov.oleksii.la@gmail.com>
2024-09-02 23:58:22 +0300

    change constants

--- settings/constants.py
+++ settings/constants.py
@@ -10,10 +10,10 @@ MODELS_DIR = 'models'
 
 TRAIN_CSV = os.path.join(ROOT_DIR, DATA_DIR, 'train.csv')
 VAL_CSV = os.path.join(ROOT_DIR, DATA_DIR, 'test.csv')
-SAVED_ESTIMATOR = os.path.join(ROOT_DIR, MODELS_DIR, 'CatBoostClassififer.pkl')
+SAVED_MODEL = os.path.join(ROOT_DIR, MODELS_DIR, 'CatBoostClassififer.pkl')
 SAVED_SCALER = os.path.join(ROOT_DIR, MODELS_DIR, 'scaler.pkl')
 
 # print(TRAIN_CSV)
 # print(VAL_CSV)
-print(SAVED_ESTIMATOR)
+#print(SAVED_ESTIMATOR)
 
